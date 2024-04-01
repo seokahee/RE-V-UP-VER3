f46a98f4d1efc13c7dfa991ac3829bfaf4ed9a48
yeji Sung <dpwl0332@naver.com>
2024-04-01 22:45:54 +0900

    Init : 초기설정

--- src/shared/store.ts
+++ src/shared/store.ts
@@ -1,1 +1,12 @@
 // store
+import { create } from "zustand";
+
+type Store = {
+  count: number;
+  inc: () => void;
+};
+
+const useStore = create<Store>()((set) => ({
+  count: 1,
+  inc: () => set((state) => ({ count: state.count + 1 })),
+}));
