668aaacaef6d0e7424343766d24b91452bba451e
HaeRheeNam <4ran0318@gmail.com>
2024-04-02 16:21:14 +0900

    feat()

--- src/app/(nonAuth)/join/page.tsx
+++ src/app/(nonAuth)/join/page.tsx
@@ -1,11 +1,99 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React, { ChangeEvent, useState } from "react";
 
 const Join = () => {
+  const joinState = {
+    userEmail: "",
+    userPw: "",
+    userPwCheck: "",
+    userNickname: "",
+  };
+  const [join, setJoin] = useState(joinState);
+
+  const validatePassword = !(join.userPw === join.userPwCheck);
+  const validatePwLength = join.userPw.length < 6;
+  const validateEmptyValue = !(
+    join.userEmail ||
+    join.userPwCheck ||
+    join.userNickname
+  );
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setJoin((preForm) => ({ ...preForm, [name]: value }));
+  };
+
+  const onJoinHandler = () => {
+    if (validateEmptyValue) {
+      alert("빈칸 없이 작성해 주세요.");
+    }
+    if (validatePassword) {
+      alert("비밀번호를 다시 입력해주세요.");
+      return;
+    }
+    if (validatePwLength) {
+      alert("비밀번호는 최소 6글자 이상 작성해 주세요");
+      return;
+    }
+  };
+
   return (
     <div>
-      Join
-    </div>
-  )
-}
+      <div>
+        <h2>회원가입</h2>
+      </div>
+      <form>
+        <div>
+          <input
+            type="email"
+            name="userEmail"
+            placeholder="이메일"
+            value={join.userEmail}
+            onChange={onChangeHandler}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="userPw"
+            placeholder="비밀번호"
+            value={join.userPw}
+            onChange={onChangeHandler}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="userPwCheck"
+            placeholder="비밀번호 확인"
+            value={join.userPwCheck}
+            onChange={onChangeHandler}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="userNickname"
+            placeholder="닉네임"
+            value={join.userNickname}
+            onChange={onChangeHandler}
+          />
+        </div>
+        <div>
+          <button onClick={onJoinHandler}>다음</button>
+        </div>
+      </form>
 
-export default Join
+      <div className="flex gap-[10px]">
+        <p>이미 계정이 있으신가요?</p>
+        <Link href={"/login"} className="font-bold">
+          로그인하기
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
--- src/app/(nonAuth)/login/page.tsx
+++ src/app/(nonAuth)/login/page.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+"use client";
 
 const LoginPage = () => {
-  return <div>page</div>;
+  return <div>로그인 페이지</div>;
 };
 
 export default LoginPage;
