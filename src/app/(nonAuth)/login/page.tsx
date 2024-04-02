27fd59a7bcd0628d6b0b7280b61fbea57399dd5b
HaeRheeNam <4ran0318@gmail.com>
2024-04-03 01:17:36 +0900

    feat(#23): 로그인 커스텀 로직 수정

--- src/app/(nonAuth)/login/page.tsx
+++ src/app/(nonAuth)/login/page.tsx
@@ -1,7 +1,45 @@
+"use client";
+
+import useInput from "@/hooks/useInput";
 import React from "react";
 
 const LoginPage = () => {
-  return <div>로그인페이쥐</div>;
+  const loginInfo = { email: "", password: "" };
+  const {
+    form: userlogin,
+    setForm: setUserlogin,
+    onChange: onChangeHandler,
+    reset,
+  } = useInput(loginInfo);
+
+  return (
+    <div>
+      <section>
+        <div>
+          <p>로그인</p>
+        </div>
+        <form>
+          <div>
+            <input
+              type="email"
+              onChange={onChangeHandler}
+              placeholder="이메일을 입력하세요"
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              onChange={onChangeHandler}
+              placeholder="비밀번호를 입력하세요"
+            />
+          </div>
+          <div>
+            <button>로그인</button>
+          </div>
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export default LoginPage;
