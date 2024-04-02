52358e09137e2e88a9fc7b3f56e0c57a1d31b89b
HaeRheeNam <4ran0318@gmail.com>
2024-04-03 01:30:44 +0900

    feat(#23): 로그인 커스텀 로직 수정

--- src/app/(nonAuth)/login/page.tsx
+++ src/app/(nonAuth)/login/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import useInput from "@/hooks/useInput";
-import React from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { FormEvent } from "react";
 
 const LoginPage = () => {
+  const router = useRouter();
   const loginInfo = { email: "", password: "" };
   const {
     form: userlogin,
@@ -11,6 +14,24 @@ const LoginPage = () => {
     onChange: onChangeHandler,
     reset,
   } = useInput(loginInfo);
+  const { email, password } = userlogin;
+
+  const onLoginHandler = async (e: FormEvent) => {
+    e.preventDefault();
+    await signIn("credentials", {
+      email: email,
+      password: password,
+      redirect: false,
+    }).then((result) => {
+      console.log(result);
+      if (result?.ok) {
+        router.push("/");
+      }
+      if (result?.error) {
+        console.error("로그인 에러입니다.");
+      }
+    });
+  };
 
   return (
     <div>
@@ -18,10 +39,12 @@ const LoginPage = () => {
         <div>
           <p>로그인</p>
         </div>
-        <form>
+        <form onSubmit={onLoginHandler}>
           <div>
             <input
               type="email"
+              name="email"
+              value={email}
               onChange={onChangeHandler}
               placeholder="이메일을 입력하세요"
             />
@@ -29,12 +52,14 @@ const LoginPage = () => {
           <div>
             <input
               type="password"
+              name="password"
+              value={password}
               onChange={onChangeHandler}
               placeholder="비밀번호를 입력하세요"
             />
           </div>
           <div>
-            <button>로그인</button>
+            <button type="submit">로그인</button>
           </div>
         </form>
       </section>
