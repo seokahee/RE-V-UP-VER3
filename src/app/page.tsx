013a654c72deb00679853dd4ff24ba1db1396711
jisu <ahteostnqls@naver.com>
2024-04-03 16:00:13 +0900

    Feat(#27) : 슬라이드 배너 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import MainBanner from "@/components/main/MainBanner";
 
 export default function Home() {
   return (
-    <h1>
+    <div>
       <MainBanner />
-    </h1>
+    </div>
   );
 }
--- src/components/main/MainBanner.tsx
+++ src/components/main/MainBanner.tsx
@@ -3,7 +3,7 @@
 import { supabase } from "@/shared/supabase/supabase";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 type Banner = {
   adId: string;
@@ -12,6 +12,8 @@ type Banner = {
 };
 
 const MainBanner = () => {
+  const [slide, setSlide] = useState(0);
+
   const USER_ID = "016011ee-39dc-41d4-92a1-1ea7316c55dc"; //임시 값
 
   const getBannerData = async (userId: string): Promise<Banner[]> => {
@@ -25,23 +27,54 @@ const MainBanner = () => {
     }
   };
 
-  const { data } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryFn: () => getBannerData(USER_ID),
     queryKey: ["mainBanner"],
     enabled: !!USER_ID
   });
 
+  const onClickPrevHandler = () => {
+    setSlide((prev) => prev - 1);
+  };
+
+  const onClickNextHandler = () => {
+    setSlide((prev) => prev + 1);
+  };
+
+  if (isError) {
+    return "에러 발생!!";
+  }
+
+  if (isLoading) {
+    return "로딩중";
+  }
+
   return (
     <div>
       {data && data?.[0].imageUrl.length > 0 ? (
-        <>
-          {data?.[0].imageUrl.map((item, idx) => {
-            const splitUrl = item.split("/");
-            return <Image key={splitUrl[splitUrl.length - 1]} src={item} width={1600} height={300} alt={`배너 이미지 ${idx}`} />;
-          })}
-        </>
+        <div className="relative m-4">
+          <ul className={`flex overflow-hidden transition-all`}>
+            {data?.[0].imageUrl.map((item, idx) => {
+              const splitUrl = item.split("/");
+
+              return (
+                <li key={splitUrl[splitUrl.length - 1]} className={`w-full [&_img]:w-full [&_img]:h-auto transition-opacity ${slide === idx ? "block" : "hidden"}`}>
+                  <Image src={item} width={1600} height={300} alt={`배너 이미지 ${idx}`} />
+                </li>
+              );
+            })}
+          </ul>
+          <div>
+            <button type="button" className={`absolute left-0 top-1/2 -translate-y-1/2 ${slide === 0 ? "hidden" : "block"}`} onClick={onClickPrevHandler}>
+              PREV
+            </button>
+            <button type="button" className={`absolute right-0 top-1/2 -translate-y-1/2 ${slide < data?.[0].imageUrl.length - 1 ? "block" : "hidden"}`} onClick={onClickNextHandler}>
+              NEXT
+            </button>
+          </div>
+        </div>
       ) : (
-        <div className="[&_img]:max-w-full [&_img]:h-auto">
+        <div className="[&_img]:w-full [&_img]:h-auto">
           <Image src={"https://lukvbpxaabobwpzkacow.supabase.co/storage/v1/object/public/adBanner/Group_286.png"} alt="배너 이미지" width={1670} height={254} />
         </div>
       )}
