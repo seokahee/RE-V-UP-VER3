f4719b8679b6ae31e41fed2f999910b184c150ad
YeonjinJo <hrj8441@naver.com>
2024-04-03 16:37:37 +0900

    Feat(#39): Stringify HTML elements when the submit button is clicked

--- src/components/editor/BasicEditor.tsx
+++ src/components/editor/BasicEditor.tsx
@@ -1,37 +1,65 @@
-import React, { useRef } from "react";
+import { buttonStyle, initialButtonStyle } from "@/style/editorStyle";
+import React, { useEffect, useRef } from "react";
 
 const BasicEditor = () => {
   const editorRef = useRef<HTMLDivElement>(null);
+  const editorValue = editorRef.current;
 
   const applyStyle = (style: string): void => {
-    if (editorRef.current) {
+    if (editorValue) {
       document.execCommand(style);
       focusEditor();
     }
   };
 
   const focusEditor = () => {
-    if (editorRef.current) {
-      editorRef.current.focus({ preventScroll: true });
+    if (editorValue) {
+      editorValue.focus({ preventScroll: true });
     }
   };
 
-  const buttonStyle = "btn mx-2 my-5 p-1 border border-solid border-black rounded";
+  const submitHandler = () => {
+    console.log(editorValue?.innerHTML);
+  };
 
   return (
     <>
-      <button className={buttonStyle} onClick={() => applyStyle('bold')}>B</button>
-      <button className={buttonStyle} onClick={() => applyStyle('italic')}>I</button>
-      <button className={buttonStyle} onClick={() => applyStyle('underline')}>U</button>
-      <button className={buttonStyle} onClick={() => applyStyle('strikeThrough')}>S</button>
-      <button className={buttonStyle} onClick={() => applyStyle('insertOrderedList')}>OL</button>
-      <button className={buttonStyle} onClick={() => applyStyle('insertUnorderedList')}>UL</button>
+      <button className={buttonStyle} onClick={() => applyStyle("bold")}>
+        B
+      </button>
+      <button className={buttonStyle} onClick={() => applyStyle("italic")}>
+        I
+      </button>
+      <button className={buttonStyle} onClick={() => applyStyle("underline")}>
+        U
+      </button>
+      <button
+        className={buttonStyle}
+        onClick={() => applyStyle("strikeThrough")}
+      >
+        S
+      </button>
+      <button
+        className={buttonStyle}
+        onClick={() => applyStyle("insertOrderedList")}
+      >
+        OL
+      </button>
+      <button
+        className={buttonStyle}
+        onClick={() => applyStyle("insertUnorderedList")}
+      >
+        UL
+      </button>
       <div
         id="editor"
         ref={editorRef}
-        contentEditable={true}
-        className="mx-5 p-10 border border-black border-solid rounded min-h-32 *:w-full"
+        contentEditable
+        className={`mx-5 p-10 border border-black border-solid rounded min-h-32 [&>img]:w-full`}
       ></div>
+      <button className={initialButtonStyle} onClick={submitHandler}>
+        submit
+      </button>{" "}
     </>
   );
 };
