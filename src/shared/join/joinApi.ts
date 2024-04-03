9ba817f3187b7093738116041135624f0bc9666b
HaeRheeNam <4ran0318@gmail.com>
2024-04-03 12:24:53 +0900

    feat(#22) : 회원가입 기능 구현 및 DB에 회원정보 저장

--- src/shared/join/joinApi.ts
+++ src/shared/join/joinApi.ts
@@ -8,7 +8,12 @@ export const signUp = async ({ email, password }: SignUp) => {
   });
 
   if (error) {
-    alert(error.message);
+    if (error.message.includes("unique")) {
+      alert("이미 가입된 이메일입니다.");
+    } else {
+      console.error("가입 오류:", error.message);
+      alert("가입 중 오류가 발생했습니다. 문의해주세요.");
+    }
   } else {
     alert("회원가입이 완료되었습니다.");
   }
