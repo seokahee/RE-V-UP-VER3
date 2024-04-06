597abac7f9cdf254eaf301594c22d0e6cd682e6b
jisu <ahteostnqls@naver.com>
2024-04-07 07:09:12 +0900

    Style(#99) : 모달 사이즈 수정

--- src/components/mypage/Modal.tsx
+++ src/components/mypage/Modal.tsx
@@ -10,7 +10,7 @@ const Modal = ({ title, children, onClick }: Modal) => {
   return (
     <div className="">
       <div className="fixed left-0 right-0 top-0 bottom-0 bg-black/[0.2]"></div>
-      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[300px] max-w-[80%] border border-solid border-slate-300 rounded-md bg-white">
+      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] max-w-[80%] border border-solid border-slate-300 rounded-md bg-white">
         <div className="flex justify-between p-4 border-b border-solid border-slate-300">
           <h3>{title}</h3>
           <button type="button" onClick={onClick}>
