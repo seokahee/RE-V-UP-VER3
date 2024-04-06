f5128944d360de17b9f1a0193ae592422f6231e2
jisu <ahteostnqls@naver.com>
2024-04-06 15:05:40 +0900

    Feat(#28) : 마이페이지 유저 정보 조회 및 마이플레이리스트 노래 정보 조회

--- src/components/mypage/MyInfo.tsx
+++ src/components/mypage/MyInfo.tsx
@@ -1,19 +1,29 @@
 "use client";
 
-import { getUserData } from "@/shared/mypage/api";
+import { getUserAndPlaylistData, getUserPlaylistMyData } from "@/shared/mypage/api";
 import { useStore } from "@/shared/store";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 
 const MyInfo = () => {
   const { userInfo } = useStore();
 
   const { data, isLoading, isError } = useQuery({
-    queryFn: () => getUserData(userInfo.uid),
+    queryFn: () => getUserAndPlaylistData(userInfo.uid),
     queryKey: ["mypage", userInfo.uid],
-    enabled: !!userInfo
+    enabled: !!userInfo.uid
   });
+
+  const { data: playlistMyData } = useQuery({
+    queryFn: () => getUserPlaylistMyData(data?.playlistMy?.[0].myMusicIds as string[]),
+    queryKey: ["playlistMy", data?.playlistMy],
+    enabled: !!data?.playlistMy?.length
+  });
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   if (isError) {
     return <>에러발생</>;
@@ -24,15 +34,43 @@ const MyInfo = () => {
   }
 
   return (
-    <div>
+    <section>
       <div>
         <figure className="w-5 h-5 flex overflow-hidden rounded-full bg-slate-200">
           {data?.userImage && <Image src={data?.userImage} width={150} height={150} alt={`${data?.nickname} 프로필 이미지`} />}
         </figure>
+        <button type="button">퍼스널 뮤직 진단 다시받기</button>
         {data?.nickname}
         팔로우 {data?.following.length} 팔로워 {data?.follower.length}
+        {data?.userChar?.mbti}
+        {data?.personalMusic?.resultSentence}
       </div>
-    </div>
+      <div>
+        <h2>{data?.nickname}님의 플레이리스트</h2>
+        <button type="button">전체 재생 하기</button>
+        <ul>
+          {playlistMyData?.map((item) => {
+            return (
+              <li key={item.musicId}>
+                <div>
+                  <label>
+                    <input type="checkbox" />
+                  </label>
+                  <figure>
+                    <Image src={item.thumbnail} width={56} height={56} alt={`${item.musicTitle} 앨범 이미지`} />
+                  </figure>
+                  <div className="flex flex-col">
+                    {item.musicTitle}
+                    <span>{item.artist}</span>
+                  </div>
+                </div>
+                <span>재생시간..</span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
   );
 };
 
--- src/shared/mypage/api.ts
+++ src/shared/mypage/api.ts
@@ -1,18 +1,41 @@
-import { UserInfo } from "@/types/mypage/types";
+import { PlaylistMy, UserInfo } from "@/types/mypage/types";
 import { supabase } from "../supabase/supabase";
 
-export const getUserData = async (userId: string): Promise<UserInfo> => {
+export const getUserAndPlaylistData = async (userId: string): Promise<UserInfo> => {
   try {
     const { data, error } = await supabase
       .from("userInfo")
-      .select("userId, email, userType, nickname, following, follower, userChar, mbtiOpen, personalMusicOpen, playlistOpen, postsOpen, likedPostsOpen, userImage")
+      .select(
+        "userId, email, userType, nickname, following, follower, userChar, mbtiOpen, personalMusicOpen, playlistOpen, postsOpen, likedPostsOpen, userImage, personalMusic(resultSentence), playlistMy(myMusicIds, playlistId)"
+      )
       .eq("userId", userId)
       .limit(1)
       .single();
+
+    if (data?.playlistMy.length === 0) {
+      const newData = { userId, myMusicIds: [] };
+      const { error: insertError } = await supabase.from("playlistMy").insert(newData);
+
+      if (insertError) {
+        console.error("Error inserting data:", insertError.message);
+      }
+    }
     console.log(data, "data");
     return data as UserInfo;
   } catch (error) {
     console.error(error);
     return {} as UserInfo;
+  }
+};
+
+export const getUserPlaylistMyData = async (myMusicIds: string[]): Promise<PlaylistMy[]> => {
+  try {
+    const { data, error } = await supabase.from("musicInfo").select("*").in("musicId", myMusicIds);
+
+    console.log(data, "data");
+    return data as PlaylistMy[];
+  } catch (error) {
+    console.error(error);
+    return [];
   }
 };
