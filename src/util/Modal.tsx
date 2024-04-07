99d5fcb207b958be56b0513211f238c224570fe9
HaeRheeNam <4ran0318@gmail.com>
2024-04-07 18:10:01 +0900

    Style : 모달 수정 및 communityDetail 틀 작성

--- src/util/Modal.tsx
+++ src/util/Modal.tsx
@@ -10,7 +10,7 @@ const Modal = ({ isOpen, children, closeModal }: ModalProps) => {
   return (
     //모달
     <section
-      className={`${isOpen ? "relative top-0 left-0 flex justify-center items-center max-w-[400px] bg-neutral-900/70 z-50" : "hidden"}`}
+      className={`${isOpen ? "relative top-0 left-0 flex justify-center items-center max-w-[400px] z-50 " : "hidden"}`}
     >
       {isOpen && (
         <>
@@ -23,7 +23,7 @@ const Modal = ({ isOpen, children, closeModal }: ModalProps) => {
               onClick={closeModal}
               className="absolute top-0 right-0 p-5 w-full h-[60px] text-black flex justify-center"
             >
-              <p className="flex bg-green-400 w-full h-[10px] font-bold">X</p>
+              <p className="flex w-full h-[10px] font-bold">X</p>
             </button>
             <div className="mt-4 rounded-[30px] bg-white w-3/5 h-4/5 max-w-7xl p-5 border-solid border-2 border-white">
               {children}
