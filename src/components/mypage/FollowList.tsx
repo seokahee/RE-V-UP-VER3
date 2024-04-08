534740cf734df2a292ef9f6befc43ae753f34f09
jisu <ahteostnqls@naver.com>
2024-04-08 22:10:44 +0900

    Feat(#2) : 팔로우/언팔로우 기능 작업

--- src/components/mypage/FollowList.tsx
+++ src/components/mypage/FollowList.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { getFollowData } from "@/shared/mypage/api";
-import { useQuery } from "@tanstack/react-query";
+import { getFollowData, updateFollow } from "@/shared/mypage/api";
+import { useStore } from "@/shared/store";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -9,9 +10,13 @@ import React from "react";
 type FollowProps = {
   data: string[];
   dataKey: string;
+  myFollowing?: string[];
 };
 
-const FollowList = ({ data, dataKey }: FollowProps) => {
+const FollowList = ({ data, dataKey, myFollowing }: FollowProps) => {
+  const { userInfo } = useStore();
+  const queryClient = useQueryClient();
+
   const {
     data: followData,
     isLoading,
@@ -21,6 +26,30 @@ const FollowList = ({ data, dataKey }: FollowProps) => {
     queryKey: [dataKey],
     enabled: !!data
   });
+
+  const unFollowMutation = useMutation({
+    mutationFn: updateFollow,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["mypage"] });
+    }
+  });
+
+  const onClickUnFollow = (userId: string, targetId: string) => {
+    const newData = data.filter((item) => item !== targetId);
+    const targetUserData = followData?.find((item) => item.userId === targetId)?.follower;
+    const newTargetData = targetUserData?.filter((item) => item !== userId)!;
+
+    unFollowMutation.mutate({ userId, targetId, followingData: newData, newTargetData });
+  };
+
+  const onClickFollow = (userId: string, targetId: string) => {
+    const newData = myFollowing?.slice()!;
+    newData.push(targetId);
+    const newTargetData = followData?.find((item) => item.userId === targetId)?.follower!;
+    newTargetData?.push(userId);
+
+    unFollowMutation.mutate({ userId, targetId, followingData: newData, newTargetData });
+  };
 
   if (isError) {
     return "에러가 발생했습니다!";
@@ -34,9 +63,36 @@ const FollowList = ({ data, dataKey }: FollowProps) => {
     <ul className="list-none">
       {followData?.map((item) => {
         return (
-          <li key={item.userId}>
-            <figure>{item.userImage && <Image src={item.userImage} width={50} height={50} alt={`${item.nickname} 프로필 이미지`} />}</figure>
-            <Link href={`/userpage/${item.userId}`}>{item.nickname}</Link>
+          <li key={item.userId} className="flex justify-between">
+            <div>
+              <figure>{item.userImage && <Image src={item.userImage} width={50} height={50} alt={`${item.nickname} 프로필 이미지`} />}</figure>
+              <Link href={`/userpage/${item.userId}`}>{item.nickname}</Link>
+            </div>
+            {dataKey === "following" ? (
+              <>
+                {data.find((el) => el === item.userId) ? (
+                  <button type="button" onClick={() => onClickUnFollow(userInfo.uid, item.userId)} className="text-red-600">
+                    팔로잉 취소
+                  </button>
+                ) : (
+                  <button type="button" onClick={() => onClickFollow(userInfo.uid, item.userId)}>
+                    팔로우
+                  </button>
+                )}
+              </>
+            ) : (
+              <>
+                {myFollowing && myFollowing.find((el) => el === item.userId) ? (
+                  <button type="button" onClick={() => onClickUnFollow(userInfo.uid, item.userId)} className="text-red-600">
+                    팔로잉 취소
+                  </button>
+                ) : (
+                  <button type="button" onClick={() => onClickFollow(userInfo.uid, item.userId)}>
+                    팔로우
+                  </button>
+                )}
+              </>
+            )}
           </li>
         );
       })}
