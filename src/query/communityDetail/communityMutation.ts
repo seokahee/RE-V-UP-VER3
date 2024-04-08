dba8e115a3b33d034aa483689ca00566bca790e4
HaeRheeNam <4ran0318@gmail.com>
2024-04-09 02:12:39 +0900

    fix(#64) : 커뮤니티 등록, 조회, 수정, 삭제에 필요한 쿼리 키, 함수 정리

--- src/query/communityDetail/communityMutation.ts
+++ src/query/communityDetail/communityMutation.ts
@@ -1,52 +1,52 @@
-// import { queryClient } from "@/app/provider";
-// import { useMutation, useQuery } from "@tanstack/react-query";
-// import { COMMUNITY_QUERY_KEY } from "./communityQueryKey";
+import { queryClient } from "@/app/provider";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { COMMUNITY_QUERY_KEY } from "./communityQueryKey";
 
-// export const useCommunityQuery = (uid:string) => {
-//   const readCommunityDetailQuery: unknown = useQuery({
-//     queryKey: [COMMUNITY_QUERY_KEY.READ_BOARD, uid],
-//     queryFn: readCommunityDetail,
-//   });
-//   return { readCommunityDetailQuery };
-// };
+export const useCommunityQuery = (uid: string) => {
+  const readCommunityDetailQuery: unknown = useQuery({
+    queryKey: [COMMUNITY_QUERY_KEY.READ_BOARD, uid],
+    queryFn: readCommunityDetail,
+  });
+  return { readCommunityDetailQuery };
+};
 
-// export const useMutationItem = () => {
-//   const addCommunityMutation = useMutation({
-//     mutationFn: addCommnityBoard,
-//     onSuccess: () =>
-//       queryClient.invalidateQueries({
-//         queryKey: [COMMUNITY_QUERY_KEY.ADD_BOARD],
-//       }),
-//     onError: () => {
-//       console.error("오류가 발생했습니다.");
-//     },
-//   });
+export const useMutationItem = () => {
+  const addCommunityMutation = useMutation({
+    mutationFn: addCommnityBoard,
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: [COMMUNITY_QUERY_KEY.ADD_BOARD],
+      }),
+    onError: () => {
+      console.error("오류가 발생했습니다.");
+    },
+  });
 
-//   const updateCommunityMutation = useMutation({
-//     mutationFn: updateCommnityBoard,
-//     onSuccess: () =>
-//       queryClient.invalidateQueries({
-//         queryKey: [COMMUNITY_QUERY_KEY.UPDATE_BOARD],
-//       }),
-//     onError: () => {
-//       console.error("오류가 발생했습니다.");
-//     },
-//   });
+  const updateCommunityMutation = useMutation({
+    mutationFn: updateCommnityBoard,
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: [COMMUNITY_QUERY_KEY.UPDATE_BOARD],
+      }),
+    onError: () => {
+      console.error("오류가 발생했습니다.");
+    },
+  });
 
-//   const deleteCommunityMutation = useMutation({
-//     mutationFn: deleteCommnityBoard,
-//     onSuccess: () =>
-//       queryClient.invalidateQueries({
-//         queryKey: [COMMUNITY_QUERY_KEY.DELETE_BOARD],
-//       }),
-//     onError: () => {
-//       console.error("오류가 발생했습니다.");
-//     },
-//   });
+  const deleteCommunityMutation = useMutation({
+    mutationFn: deleteCommnityBoard,
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: [COMMUNITY_QUERY_KEY.DELETE_BOARD],
+      }),
+    onError: () => {
+      console.error("오류가 발생했습니다.");
+    },
+  });
 
-//   return {
-//     addCommunityMutation,
-//     updateCommunityMutation,
-//     deleteCommunityMutation,
-//   };
-// };
+  return {
+    addCommunityMutation,
+    updateCommunityMutation,
+    deleteCommunityMutation,
+  };
+};
