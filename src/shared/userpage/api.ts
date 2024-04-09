259daa2260cad1d3f5966e928f1879114c319a7f
jisu <ahteostnqls@naver.com>
2024-04-10 05:08:41 +0900

    Feat(#86) : 공개/비공개 정보 조회 로직 추가

---
+++ src/shared/userpage/api.ts
@@ -0,0 +1,21 @@
+import { supabase } from '../supabase/supabase'
+
+export const getUserVisibilityData = async (userId: string) => {
+  try {
+    const { data, error } = await supabase
+      .from('userInfo')
+      .select(
+        'userId, mbtiOpen, personalMusicOpen,playlistOpen, postsOpen,likedPostsOpen',
+      )
+      .eq('userId', userId)
+      .single()
+    if (error) {
+      console.error(error)
+      return
+    }
+
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
