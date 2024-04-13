cca305d8849b40da35420f1f9bf6dc7744118cde
jisu <ahteostnqls@naver.com>
2024-04-14 04:27:31 +0900

    Style(#170) : 메인 배너 슬라이드 ui 작업

--- src/components/main/MainBanner.tsx
+++ src/components/main/MainBanner.tsx
@@ -17,38 +17,58 @@ const MainBanner = () => {
   const bannerArray = [
     {
       id: 1,
-      src: 'https://hxavgjouatzlrjtjgrth.supabase.co/storage/v1/object/public/adBanner/ban1.png?t=2024-04-13T17%3A12%3A29.024Z',
+      src: 'https://hxavgjouatzlrjtjgrth.supabase.co/storage/v1/object/public/adBanner/1.png',
     },
     {
       id: 2,
-      src: 'https://hxavgjouatzlrjtjgrth.supabase.co/storage/v1/object/public/adBanner/ban2.png',
+      src: 'https://hxavgjouatzlrjtjgrth.supabase.co/storage/v1/object/public/adBanner/2.png',
     },
   ]
 
+  const itemShadow =
+    'shadow-[0px_4px_1px_-1px_#00000033,0px_0px_0px_1px_#ffffff26,inset_0px_2px_0px_#ffffff1a,inset_0px_-1px_2px_#00000033,inset_0px_-4px_1px_#00000033,_4px_4px_3px_0px_#0000004c] drop-shadow-[0_4px_8px_rgba(0,0,0,0.1)]'
+
   return (
     <div className='relative m-4'>
-      <ul className='flex overflow-hidden rounded-[2rem] transition-all'>
+      <ul
+        className={`flex overflow-hidden rounded-[2rem] border-4 border-black transition-all ${itemShadow}`}
+      >
         {bannerArray.map((item, idx) => {
           return (
             <li
               key={item.id}
-              className={`w-full transition-opacity [&_img]:h-auto [&_img]:w-full ${slide === idx ? 'block' : 'hidden'}`}
+              className={`w-full transition-opacity [&_img]:h-auto [&_img]:w-full ${slide === idx ? 'block' : 'hidden'} `}
             >
               <Image
                 src={item.src}
-                width={1600}
-                height={300}
+                width={732}
+                height={180}
                 alt={`배너 이미지 ${idx}`}
               />
             </li>
           )
         })}
       </ul>
-      <div className='absolute bottom-6 right-6'>
-        <button type='button' className='' onClick={onClickPrevHandler}>
+      <div className='absolute bottom-6 right-6 min-w-[69px]  rounded-full bg-[#ffffffb2] px-[6px] py-[2px] text-center text-[12px] backdrop-blur-sm'>
+        <button
+          type='button'
+          className={`${slide === 0 ? '' : 'text-white'} px-[2px]`}
+          onClick={onClickPrevHandler}
+          disabled={slide === 0 ? true : false}
+        >
           &lt;
         </button>
-        <button type='button' className='' onClick={onClickNextHandler}>
+        <span className='px-[2px] font-bold text-white'>{slide + 1}</span>
+        <span className='px-[2px] text-white'>/</span>
+        <span className='px-[2px] font-bold text-[#ffffff7f]'>
+          {bannerArray.length}
+        </span>
+        <button
+          type='button'
+          className={`${slide < bannerArray.length - 1 ? '' : 'text-white'}  px-[2px]`}
+          onClick={onClickNextHandler}
+          disabled={slide < bannerArray.length - 1 ? false : true}
+        >
           &gt;
         </button>
       </div>
