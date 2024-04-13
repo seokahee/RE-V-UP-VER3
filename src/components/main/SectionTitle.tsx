f0c188adec77fee56c3d0d48ca42f037ebbf7b88
jisu <ahteostnqls@naver.com>
2024-04-14 04:53:46 +0900

    Style(#174) : 메인 타이틀 컴포넌트로 분리

---
+++ src/components/main/SectionTitle.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SectionTitle = ({ children }: { children: React.ReactNode }) => {
+  return <h2 className='mb-5 text-xl font-bold'>{children}</h2>
+}
+
+export default SectionTitle
