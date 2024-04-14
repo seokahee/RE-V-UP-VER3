0aea45e1043c06f35ea35e2a2588cfabf40b45c6
서가희 <kahee430@gmail.com>
2024-04-15 03:42:56 +0900

    style(#180):게시글 검색 리스트 UI작업중

--- src/components/search/SearchedCommunityData.tsx
+++ src/components/search/SearchedCommunityData.tsx
@@ -7,19 +7,24 @@ const SearchedCommunityData = () => {
   const { searchedData } = useSearchedResultStore()
   const { communityData } = searchedData
   return (
-    <div>
+    <div className='flex h-[440px] w-[732px] flex-col'>
       {communityData.map((item) => {
         return (
-          <Link
-            href={`/community/${item.boardId}`}
-            className='block overflow-hidden text-ellipsis whitespace-nowrap'
-          >
-            <div key={item.boardId} className='flex items-center'>
-              <img src={item.userInfo.userImage} alt='' className='w-28' />
-              <img src={item.musicInfo.thumbnail} alt='' className='w-28' />
-              <div className='flex flex-col gap-2'>
+          <Link href={`/community/${item.boardId}`}>
+            <div key={item.boardId} className='flex h-[80px] w-[620px]'>
+              <img
+                src={item.userInfo.userImage}
+                alt={`${item.userInfo.nickname}님의 이미지`}
+                className='w-28'
+              />
+              <img
+                src={item.musicInfo.thumbnail}
+                alt='앨범 썸네일'
+                className='w-28'
+              />
+              <div>
                 <div>{item.boardTitle}</div>
-                <div className='flex gap-2'>
+                <div>
                   <div>{item.userInfo.nickname}</div>
                   <div>{onDateHandler(item.date)}</div>
                   <div>좋아요 {item.likeList.length}</div>
