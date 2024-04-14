e58eba7dd7c0cfec717fc98e783768f390ad12ac
yeji Sung <dpwl0332@naver.com>
2024-04-14 21:25:11 +0900

    Style(#184) : 헤더/푸터 디자인 작업

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,28 +9,30 @@ const Footer = () => {
 
   return (
     <footer className={`${!check ? 'pl-10' : 'pl-20'} border-t border-black`}>
-      <nav className='footer  p-8 '>
+      <nav className='footer py-8'>
         <section>
-          <p>V-UP</p>
+          <p className='py-2 text-4xl font-bold italic'>V-UP</p>
         </section>
-        <section className=' flex flex-row'>
-          <section className='basis-1/2'>
-            <section className=' flex flex-row gap-8'>
-              <section className=' w-40'>front-end Developer</section>
-              <ul className=' grid list-none grid-cols-5 gap-4'>
+        <section className='flex flex-row py-2'>
+          <section className=''>
+            <section className='flex flex-row gap-8 py-2'>
+              <section className='w-40 text-xs'>front-end Developer</section>
+              <ul className='grid list-none grid-cols-5 gap-3 text-xs'>
                 <li>성예지</li>
-                <li>남해리</li>
                 <li>강지수</li>
+                <li>남해리</li>
                 <li>서가희</li>
               </ul>
             </section>
-            <section className=' flex flex-row gap-8'>
-              <p className='w-40'>UXUI Designer</p>
-              <p className=''>전주용</p>
+            <section className='flex flex-row gap-8 '>
+              <p className='w-40  text-xs'>UXUI Designer</p>
+              <p className=' text-xs'>전주용</p>
             </section>
           </section>
-          <section className=' basis-1/2'>
-            <p>© 2024 VakVakVerse. All rights reserved.</p>
+          <section className='flex justify-end '>
+            <p className='justify-end py-2  text-xs text-primary'>
+              © 2024 VakVakVerse. All rights reserved.
+            </p>
           </section>
         </section>
       </nav>
