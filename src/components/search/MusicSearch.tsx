815757334979b5a48ab0a9f608eab1296729709a
HaeRheeNam <4ran0318@gmail.com>
2024-04-15 05:59:52 +0900

    Style(#214) : 커뮤니티 상세 페이지 - 등록 (음악 검색 등록)

--- src/components/search/MusicSearch.tsx
+++ src/components/search/MusicSearch.tsx
@@ -1,5 +1,7 @@
 import { MouseEvent, useState } from 'react'
 import MusicSearchModal from './MusicSearchModal'
+import { DOWN_ACTIVE_BUTTON } from '../login/loginCss'
+import { ACTIVE_BUTTON_SHADOW } from '../login/buttonCss'
 
 const MusicSearch = () => {
   const [isModal, setIsModal] = useState<boolean>(false)
@@ -10,13 +12,20 @@ const MusicSearch = () => {
   }
 
   return (
-    <div>
-      <button onClick={onModalOpenHandler}>음악 등록하기</button>
-      {isModal && (
-        <div>
-          <MusicSearchModal isModal={isModal} setIsModal={setIsModal} />
-        </div>
-      )}
+    <div className='flex flex-col justify-center gap-[16px]'>
+      <div className='flex'>
+        <button
+          onClick={onModalOpenHandler}
+          className={`flex h-[88px] w-[114px] items-center justify-center rounded-[12px] bg-primary text-[16px] font-bold active:bg-[rgba(104,91,255,0.20)] ${DOWN_ACTIVE_BUTTON} ${ACTIVE_BUTTON_SHADOW} `}
+        >
+          음악 등록하기
+        </button>
+        {isModal && (
+          <div>
+            <MusicSearchModal isModal={isModal} setIsModal={setIsModal} />
+          </div>
+        )}
+      </div>
     </div>
   )
 }
