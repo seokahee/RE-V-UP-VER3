8fbf5246fd2deffa70b2920036b28caf71c00db2
jisu <ahteostnqls@naver.com>
2024-04-15 02:54:43 +0900

    Feat(#199) : 미 로그인 시 접근 불가하도록 처리

--- src/app/(auth)/mypage/page.tsx
+++ src/app/(auth)/mypage/page.tsx
@@ -4,18 +4,38 @@ import LikeBoardList from '@/components/mypage/LikeBoardList'
 import MyInfo from '@/components/mypage/MyInfo'
 import TabMenu from '@/components/mypage/TabMenu'
 import WriteList from '@/components/mypage/WriteList'
+import { useSession } from 'next-auth/react'
+import { usePathname, useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 const MyPage = () => {
+  const session = useSession()
+  const router = useRouter()
+  const pathname = usePathname()
+  const [shouldRender, setShouldRender] = useState(false)
+
   const tabArr = [
     { id: 0, title: '내 프로필', content: <MyInfo /> },
     { id: 1, title: '내가 쓴 글', content: <WriteList /> },
     { id: 3, title: '좋아요 한 글', content: <LikeBoardList /> },
   ]
 
+  useEffect(() => {
+    if (session.status !== 'authenticated') {
+      router.replace('/')
+    } else {
+      setShouldRender(true)
+    }
+  }, [router, session, pathname])
+
   return (
-    <div>
-      <TabMenu data={tabArr} width='w-1/3' />
-    </div>
+    <>
+      {shouldRender && (
+        <div>
+          <TabMenu data={tabArr} width='w-1/3' />
+        </div>
+      )}
+    </>
   )
 }
 
