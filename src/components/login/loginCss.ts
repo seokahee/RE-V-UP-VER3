eb79056daf2bb40bde467e943300c8815303d553
HaeRheeNam <4ran0318@gmail.com>
2024-04-15 20:20:43 +0900

    Style(#232) : 로그인 css - 로그인 인풋 shadow 값 수정

--- src/components/login/loginCss.ts
+++ src/components/login/loginCss.ts
@@ -1,5 +1,5 @@
 export const SHADOW =
-  'shadow-[0px_1px_2px_0px_rgba(255,255,255,0.20)_inset,_0px_4px_4px_0px_rgba(0,0,0,0.20),_0px_0px_0px_1px_rgba(0,0,0,0.10),_0px_0px_0px_4px_rgba(0,0,0,0.10),0px_0px_0px_3px_rgba(255,255,255,0.20),_0px_4px_1px_0px_rgba(255,255,255,0.20)_inset,_0px_-4px_1px_0px_rgba(0,0,0,0.10)_inset]'
+  'shadow-[0px_1px_2px_0px_rgba(255,255,255,0.20)_inset,_0px_4px_4px_0px_rgba(0,0,0,0.20),_0px_0px_0px_1px_rgba(0,0,0,0.10),0px_0px_0px_4px_rgba(0,0,0,0.10),0px_0px_0px_3px_rgba(255,255,255,0.20),0px_4px_1px_0px_rgba(255,255,255,0.20)_inset,0px_-4px_1px_0px_rgba(0,0,0,0.10)_inset]'
 
 export const INPUT_SHADOW =
   'shadow-[0px_4px_1px_rgba(0,0,0,0.1),0px_-4px_4px_rgba(255,255,255,0.1),0px_0px_0px_3px_rgba(255,255,255,0.2),inset_0px_1px_2px_rgba(0,0,0,0.1),inset_0px_-4px_1px_rgba(255,255,255,0.05),inset_0px_4px_1px_rgba(0,0,0,0.3)]'
@@ -13,4 +13,4 @@ export const DOWN_ACTIVE_BUTTON =
   'active:text-[rgba(255,255,255,0.3)] active:drop-shadow-none active:shadow-[0px_1px_2px_0px_rgba(0,0,0,0.20)_inset,0px_4px_4px_0px_rgba(255,255,255,0.20),0px_0px_0px_1px_rgba(0,0,0,0.10),0px_0px_0px_4px_rgba(104,91,255,0.10),0px_0px_0px_3px_rgba(255,255,255,0.20),0px_4px_1px_0px_rgba(0,0,0,0.20)_inset,0px_-4px_1px_0px_rgba(255,255,255,0.05)_inset]'
 
 export const OPEN_ANOTHER_SHADOW =
-  'shadow-[0px_4px_4px_rgba(0, 0, 0, 0.2),0px_0px_0px_1px_rgba(0, 0, 0, 0.1),0px_0px_0px_4px_rgba(0, 0, 0, 0.1),0px_0px_0px_3px_rgba(255, 255, 255, 0.2),inset_0px_1px_2px_rgba(255, 255, 255, 0.2),inset_0px_4px_1px_rgba(255, 255, 255, 0.2),inset_0px_-4px_1px_rgba(0, 0, 0, 0.1)]'
+  'shadow-[0px_4px_4px_rgba(0,0,0,0.2),0px_0px_0px_1px_rgba(0,0,0,0.1),0px_0px_0px_4px_rgba(0,0,0,0.1),0px_0px_0px_3px_rgba(255,255,255,0.2),inset_0px_1px_2px_rgba(255,255,255,0.2),inset_0px_4px_1px_rgba(255,255,255,0.2),inset_0px_-4px_1px_rgba(0,0,0,0.1)]'
