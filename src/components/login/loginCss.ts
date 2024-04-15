1f37e0287db23cc783c11d88f6ac5da902a8f280
HaeRheeNam <4ran0318@gmail.com>
2024-04-16 00:51:43 +0900

    Style(#233) : 로그인 css - 로그인 인풋 placeholeder 컬러 복원

--- src/components/login/loginCss.ts
+++ src/components/login/loginCss.ts
@@ -4,7 +4,7 @@ export const SHADOW =
 export const INPUT_SHADOW =
   'shadow-[0px_4px_1px_rgba(0,0,0,0.1),_0px_-4px_4px_rgba(255,255,255,0.1),_0px_0px_0px_3px_rgba(255,255,255,0.2),_inset_0px_1px_2px_rgba(0,0,0,0.1),_inset_0px_-4px_1px_rgba(255,255,255,0.05),_inset_0px_4px_1px_rgba(0,0,0,0.3)]'
 
-export const DROP_SHADOW = 'drop-shadow-[0px_4px_2px_rgba(0,0,0,0.5)]'
+export const DROP_SHADOW = 'drop-shadow-[0px_4px_1px_rgba(0,0,0,0.5)]'
 
 export const INPUT_FOCUS =
   'focus:text-white focus:drop-shadow-none focus:border-primary focus:outline-none'
