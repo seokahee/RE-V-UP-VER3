b59e48fac026c9123b96a0160d3d0a45c6ac98a1
jisu <ahteostnqls@naver.com>
2024-04-17 19:40:18 +0900

    Style(#272) : 노출할 데이터 없을 때 나오는 ui 작업

--- src/components/mypage/BoardNoData.tsx
+++ src/components/mypage/BoardNoData.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BoardNoData = () => {
   return (
-    <li className='flex h-[300px] items-center justify-center text-2xl font-bold text-white/60'>
+    <li className='flex h-[300px] items-center justify-center text-[1rem] font-bold text-white/50'>
       게시글이 없습니다.
     </li>
   )
