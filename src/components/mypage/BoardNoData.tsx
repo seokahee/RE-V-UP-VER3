3d3623830d8cbaf32cd9c356b858c85467e8bd42
jisu <ahteostnqls@naver.com>
2024-04-17 19:36:23 +0900

    Style(#272) : 좋아요한글, 작성한 글 없을 때 ui 작업

--- src/components/mypage/BoardNoData.tsx
+++ src/components/mypage/BoardNoData.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const BoardNoData = () => {
-  return <li>데이터가 없습니다.</li>
+  return (
+    <li className='flex h-[300px] items-center justify-center text-2xl font-bold text-white/60'>
+      게시글이 없습니다.
+    </li>
+  )
 }
 
 export default BoardNoData
