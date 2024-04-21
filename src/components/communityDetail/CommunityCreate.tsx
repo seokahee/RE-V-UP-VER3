b170960bf4b770eec3f15406d075756b7e3961c1
HaeRheeNam <4ran0318@gmail.com>
2024-04-22 01:45:03 +0900

    Refactor(#13) : 음악 등록&조회 컴포넌트 분리

--- src/components/communityDetail/CommunityCreate.tsx
+++ src/components/communityDetail/CommunityCreate.tsx
@@ -9,19 +9,15 @@ import {
   useCoummunityItem,
   validateFormBlank,
 } from '@/query/communityDetail/mutation'
-import MusicSearch from '../search/MusicSearch'
 import useInput from '@/hooks/useInput'
 import goback from '@/../public/images/goback.svg'
-import {
-  ADDED_CURRENT_MUSIC_SHADOW,
-  ADD_BOARD_STICK,
-  ALLOW_SHADOW,
-} from './communityCss'
+import { ADD_BOARD_STICK, ALLOW_SHADOW } from './communityCss'
 import { DOWN_ACTIVE_BUTTON } from '../login/loginCss'
 import { ACTIVE_BUTTON_SHADOW } from '../login/buttonCss'
 import Swal from 'sweetalert2'
 import CommunityQuillEditor from './CommunityQuillEditor'
 import ReactQuill from 'react-quill'
+import CommunityAddMusic from './CommunityAddMusic'
 
 type CommunityForm = {
   boardTitle: string
@@ -201,44 +197,11 @@ const CommunityCreate = () => {
         </div>
       </form>
 
-      <article className='flex flex-col gap-[16px]'>
-        <section className='flex gap-[16px]'>
-          <MusicSearch />
-          <article
-            className={`flex h-[88px] w-[602px] gap-[16px] rounded-[16px] bg-[rgba(255,255,255,0.1)] p-[16px] ${ADDED_CURRENT_MUSIC_SHADOW} flex gap-[16px]`}
-          >
-            <section className='flex gap-[16px]'>
-              <div className='flex items-center'>
-                {thumbnail ? (
-                  <Image
-                    src={thumbnail}
-                    alt='노래앨범이미지'
-                    width={56}
-                    height={56}
-                    className='rounded-full border-[2px] border-solid border-[rgba(255,255,255,0.1)]'
-                  />
-                ) : (
-                  <div className='h-[56px] w-[56px] rounded-full border-[2px] border-solid border-[rgba(255,255,255,0.1)]'>
-                    <i></i>
-                  </div>
-                )}
-              </div>
-              <article className='flex w-full items-center justify-center [&_div]:flex [&_div]:gap-[16px]'>
-                <div className='flex w-full items-center'>
-                  <p className='text-[24px] font-bold'>{musicTitle}</p>
-
-                  <p className='text-[16px] font-bold'>{artist}</p>
-                </div>
-              </article>
-            </section>
-          </article>
-        </section>
-        <div>
-          <p className='text-[14px] text-[rgba(255,255,255,0.5)]'>
-            게시글을 등록하기 위해 음악을 추가 해야돼요!
-          </p>
-        </div>
-      </article>
+      <CommunityAddMusic
+        thumbnail={thumbnail}
+        musicTitle={musicTitle}
+        artist={artist}
+      />
     </div>
   )
 }
