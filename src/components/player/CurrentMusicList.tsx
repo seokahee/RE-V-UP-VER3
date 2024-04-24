823659de2e17f4fc241c4a3416cef99a3fbb259e
서가희 <kahee430@gmail.com>
2024-04-24 11:29:56 +0900

    feat(#14):현재 플레이 리스트 드래그 앤 드롭으로 인덱스 변경 기능 구현중

--- src/components/player/CurrentMusicList.tsx
+++ src/components/player/CurrentMusicList.tsx
@@ -4,10 +4,10 @@ import {
   getMusicList,
 } from '@/query/musicPlayer/musicPlayerQueryKeys'
 import { insertCurrentMusic, updateCurrentMusic } from '@/shared/main/api'
-import { MusicListProps } from '@/types/musicPlayer/types'
+import { MusicInfoType, MusicListProps } from '@/types/musicPlayer/types'
 import { useMutation } from '@tanstack/react-query'
 import { useSession } from 'next-auth/react'
-import { DragEvent } from 'react'
+import { DragEvent, useState } from 'react'
 import Swal from 'sweetalert2'
 import CheckboxItem from '../mypage/CheckboxItem'
 
@@ -24,6 +24,9 @@ const CurrentMusicList = ({
   onDeleteCurrentMusicHandler,
   setMusicIndex,
 }: MusicListProps) => {
+  const [isDrag, setIsDrag] = useState(false)
+  const [currentItem, setCurrentItem] = useState(currentPlayList)
+
   const { data: userSessionInfo } = useSession()
   const uid = userSessionInfo?.user?.uid as string
 
@@ -49,34 +52,38 @@ const CurrentMusicList = ({
 
   const dropHandler = (e: DragEvent<HTMLDivElement>) => {
     e.preventDefault()
-    const musicInfo = JSON.parse(e.dataTransfer.getData('musicInfo'))
+    console.log('isDrag', isDrag)
 
-    if (playListCurrent && playListCurrent.length > 0) {
-      const currentList = playListCurrent[0].currentMusicIds
-      if (currentList.find((el) => el === musicInfo.musicId)) {
-        Swal.fire({
-          icon: 'warning',
-          title: '이미 추가된 노래입니다.',
-          showConfirmButton: false,
-          timer: 1500,
-          background: '#2B2B2B',
-          color: '#ffffff',
-        })
-        return
+    if (!isDrag) {
+      const musicInfo = JSON.parse(e.dataTransfer.getData('musicInfo'))
+
+      if (playListCurrent && playListCurrent.length > 0) {
+        const currentList = playListCurrent[0].currentMusicIds
+        if (currentList.find((el) => el === musicInfo.musicId)) {
+          Swal.fire({
+            icon: 'warning',
+            title: '이미 추가된 노래입니다.',
+            showConfirmButton: false,
+            timer: 1500,
+            background: '#2B2B2B',
+            color: '#ffffff',
+          })
+          return
+        }
+        currentList.push(musicInfo.musicId)
+        updateMutation.mutate({ userId: uid, currentList })
+      } else {
+        insertMutation.mutate({ userId: uid, musicId: musicInfo.musicId })
       }
-      currentList.push(musicInfo.musicId)
-      updateMutation.mutate({ userId: uid, currentList })
-    } else {
-      insertMutation.mutate({ userId: uid, musicId: musicInfo.musicId })
+      Swal.fire({
+        icon: 'success',
+        title: '현재 재생목록에 추가 되었습니다.',
+        showConfirmButton: false,
+        timer: 1500,
+        background: '#2B2B2B',
+        color: '#ffffff',
+      })
     }
-    Swal.fire({
-      icon: 'success',
-      title: '현재 재생목록에 추가 되었습니다.',
-      showConfirmButton: false,
-      timer: 1500,
-      background: '#2B2B2B',
-      color: '#ffffff',
-    })
   }
   const dragOverHandler = (e: DragEvent<HTMLDivElement>) => {
     e.preventDefault()
@@ -91,25 +98,78 @@ const CurrentMusicList = ({
     }
     setSelectAll((prev) => !prev)
   }
+
+  const indexDragHandler = (
+    e: DragEvent<HTMLDivElement>,
+    item: MusicInfoType,
+  ) => {
+    e.dataTransfer.setData('listItem', JSON.stringify(item))
+  }
+
+  const indexChangeDropHandler = (e: DragEvent<HTMLDivElement>) => {
+    const listItem = JSON.parse(e.dataTransfer.getData('listItem'))
+
+    const dragIndex = currentItem.findIndex(
+      (item) => item.musicId === listItem.musicId,
+    )
+
+    // 드롭된 위치 계산
+    const dropY = e.clientY
+    const dropIndex = calculateDropIndex(dropY)
+
+    if (dragIndex !== -1 && dropIndex !== -1 && dragIndex !== dropIndex) {
+      // 새로운 배열을 만들어서 드롭된 요소의 위치를 변경
+      const newPlayList = [...currentItem]
+      const [draggedItem] = newPlayList.splice(dragIndex, 1) // 드래그된 요소를 제거
+      newPlayList.splice(dropIndex, 0, draggedItem) // 드롭된 위치에 요소를 삽입
+
+      // 새로운 플레이리스트를 설정
+      setCurrentItem(newPlayList)
+    }
+  }
+
+  const calculateDropIndex = (dropY: number) => {
+    // 드래그된 아이템의 높이 계산
+    const draggedItemHeight = 63
+
+    // 목록의 상단 위치 계산
+    const listTop =
+      document.querySelector('.your-list-selector')?.getBoundingClientRect()
+        .top || 0
+
+    // 드롭된 위치를 기준으로 목록의 위치를 계산
+    const yOffset = dropY - listTop
+
+    // 드롭된 위치를 기준으로 목록에서의 인덱스를 계산
+    let currentIndex = Math.floor(yOffset / draggedItemHeight)
+
+    // 현재 인덱스가 음수인 경우
+    currentIndex = Math.max(currentIndex, 0)
+
+    // 현재 인덱스가 목록의 길이를 초과한 경우
+    currentIndex = Math.min(currentIndex, currentItem.length)
+
+    return currentIndex
+  }
   return (
     <div
       className='mt-[16px] flex max-h-[250px] min-h-[250px] flex-col overflow-y-auto overflow-x-hidden'
       onDrop={dropHandler}
       onDragOver={dragOverHandler}
     >
-      {currentPlayList.length === 0 && (
+      {currentItem.length === 0 && (
         <div className=' flex flex-col items-center text-[18px] opacity-50'>
           음악을 추가해주세요
         </div>
       )}
       <div className='flex  flex-col justify-between '>
-        {currentPlayList.map((item) => {
-          const musicIndex = currentPlayList.findIndex(
+        {currentItem.map((item) => {
+          const musicIndex = currentItem.findIndex(
             (arr) => arr.musicId === item.musicId,
           )
 
           return (
-            <div key={item.musicId}>
+            <div onDrop={indexChangeDropHandler} key={item.musicId}>
               {!isLyrics ? (
                 <div className='relative flex max-h-[63px] w-[366px] justify-between pb-[8px] pl-[16px] pr-[16px] pt-[8px]'>
                   <div className='flex items-center gap-[16px]'>
@@ -127,7 +187,7 @@ const CurrentMusicList = ({
                       <p
                         onClick={() => {
                           setMusicIndex(musicIndex)
-                          setCurrentPlaying(currentPlayList[musicIndex])
+                          setCurrentPlaying(currentItem[musicIndex])
                         }}
                         className='cursor-pointer text-[16px]'
                       >
@@ -141,18 +201,28 @@ const CurrentMusicList = ({
                   <span className='text-[14px] opacity-[30%] '>
                     {item.runTime}
                   </span>
+
+                  <p
+                    draggable='true'
+                    onDragStart={(e) => {
+                      setIsDrag(true)
+                      indexDragHandler(e, item)
+                    }}
+                  >
+                    Drag
+                  </p>
                 </div>
               ) : null}
               {isLyrics && item.musicId === currentPlaying?.musicId && (
                 <div className='m-auto  w-[326px] items-center p-[8px] text-center text-[14px] leading-[150%] opacity-[30%]'>
-                  {currentPlayList[musicIndex].lyrics}
+                  {currentItem[musicIndex].lyrics}
                 </div>
               )}
             </div>
           )
         })}
       </div>
-      {!isLyrics && currentPlayList.length > 0 && checkedList.length > 0 && (
+      {!isLyrics && currentItem.length > 0 && checkedList.length > 0 && (
         <div className='absolute bottom-[40px] left-[56px] right-[56px] flex gap-[8px]'>
           <button
             type='button'
@@ -174,3 +244,10 @@ const CurrentMusicList = ({
   )
 }
 export default CurrentMusicList
+const a = [0, 1, 3, 2]
+console.log('a', a[4])
+
+// 드랍을했을때, 들고있는 아이템과 드랍을 내려놓은곳에 아이템의 아이디를 비교해서
+// 다르면 내가 내려놓은 아이템의 인덱스를 번으로 바꿈
+// 배열의 위치에따라 인덱스를 변경하라
+// 그러면 드랍했을때 인덱스가 자동으로 재정렬
